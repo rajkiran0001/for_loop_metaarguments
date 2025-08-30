f11cd74331789a86453fc16c2e4253f2dab112df
rajkiran0001 <rajkiran.one@gmail.com>
2025-08-30 09:25:13 +0200

    Using depends on

--- main.tf
+++ main.tf
@@ -6,4 +6,17 @@ resource "aws_instance" "ec2" {
   tags = {
     Name = var.ec2_names[count.index]
   }
+  depends_on = [aws_security_group.allow_ssh]
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
